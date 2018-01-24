c1bab2120de7652f5381e859aaf9e89c74f729de
Hey <htbkoo@users.noreply.github.com>
2018-01-24 19:41:43 +0800

    added display of clock.name, clock.duration and clock.colour in <Clock/> Pomodoro Clock (with React + Redux)

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,9 +3,17 @@ import React from "react";
 import {connect} from "react-redux";
 import type {Clock} from "../redux/state";
 
+type Props = $ReadOnly<{
+    +clock: Clock
+}>
+
 // export const mapDispatchToProps = (state: State): DispatchProps => ({});
-export const ClockComponent = (): React$Element<any> => (
-    <div/>
+export const ClockComponent = (props: Props): React$Element<any> => (
+    <div>
+        <div className="clocks-list-clock-name">{props.clock.name}</div>
+        <div className="clocks-list-clock-duration">{props.clock.duration}</div>
+        <div className="clocks-list-clock-colour">{props.clock.colour}</div>
+    </div>
 );
 
 // Untested
