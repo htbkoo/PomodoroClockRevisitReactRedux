265a74e84a2df14ccb9211f8ebe1c93acb6ad12d
htbkoo <h1017760@connect.hku.hk>
2018-01-25 22:07:37 +0800

    added defaults clocks (pomodoro, break) to initialState - fulfilled feature test Pomodoro Clock (with React + Redux)

--- src/redux/state.js
+++ src/redux/state.js
@@ -95,4 +95,18 @@ export class StateBuilder {
     }
 }
 
-export const newInitialStateBuilder = (): StateBuilder => new StateBuilder().withTime(1500000).withOriginalTime(1500000).withInterval(100);
+const pomodoroClock = {
+    id: "1",
+    duration: 1500000,
+    name: "pomodoro",
+    colour: "red"
+}, breakClock = {
+    id: "2",
+    duration: 300000,
+    name: "break",
+    colour: "green"
+};
+
+export const newInitialStateBuilder = (): StateBuilder => new StateBuilder().withTime(1500000).withOriginalTime(1500000).withInterval(100)
+    .addClock(pomodoroClock)
+    .addClock(breakClock);
