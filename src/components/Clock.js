57d493c44242c6d6fca3cef815059913f2255b4f
Hey <htbkoo@users.noreply.github.com>
2018-01-27 08:16:20 +0800

    small refactor in Clock.js Pomodoro Clock (with React + Redux)

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -11,49 +11,51 @@ type Props = $ReadOnly<{
 }>
 
 // export const mapDispatchToProps = (state: State): DispatchProps => ({});
-export const ClockComponent = (props: Props): React$Element<any> => (
-    <div className="Clock">
-        <Form horizontal>
-            <FormGroup controlId="formHorizontalName" className="clocks-list-clockName">
-                <Col componentClass={ControlLabel} sm={1}/>
-                <Col sm={10}>
-                    <FormControl
-                        type="text"
-                        value={props.clock.name}
-                        placeholder="Name"
-                        onChange={NO_OP()}
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup controlId="formHorizontalDuration" className="clocks-list-clockDuration">
-                <Col componentClass={ControlLabel} sm={3}>
-                    Duration
-                </Col>
-                <Col sm={9}>
-                    <FormControl
-                        type="text"
-                        value={props.clock.duration}
-                        placeholder="Duration"
-                        onChange={NO_OP()}
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup controlId="formHorizontalColour" className="clocks-list-clockColour">
-                <Col componentClass={ControlLabel} sm={3}>
-                    Colour
-                </Col>
-                <Col sm={9}>
-                    <FormControl
-                        type="text"
-                        value={props.clock.colour}
-                        placeholder="Colour"
-                        onChange={NO_OP()}
-                    />
-                </Col>
-            </FormGroup>
-        </Form>
-    </div>
-);
+export const ClockComponent = (props: Props): React$Element<any> => {
+    return (
+        <div className="Clock">
+            <Form horizontal>
+                <FormGroup controlId="formHorizontalName" className="clocks-list-clock-name">
+                    <Col componentClass={ControlLabel} sm={1}/>
+                    <Col sm={10}>
+                        <FormControl
+                            type="text"
+                            value={props.clock.name}
+                            placeholder="Name"
+                            onChange={NO_OP()}
+                        />
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalDuration" className="clocks-list-clock-duration">
+                    <Col componentClass={ControlLabel} sm={3}>
+                        Duration
+                    </Col>
+                    <Col sm={9}>
+                        <FormControl
+                            type="text"
+                            value={props.clock.duration}
+                            placeholder="Duration"
+                            onChange={NO_OP()}
+                        />
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalColour" className="clocks-list-clock-colour">
+                    <Col componentClass={ControlLabel} sm={3}>
+                        Colour
+                    </Col>
+                    <Col sm={9}>
+                        <FormControl
+                            type="text"
+                            value={props.clock.colour}
+                            placeholder="Colour"
+                            onChange={NO_OP()}
+                        />
+                    </Col>
+                </FormGroup>
+            </Form>
+        </div>
+    )
+};
 
 // Untested
 export default connect()(ClockComponent);
