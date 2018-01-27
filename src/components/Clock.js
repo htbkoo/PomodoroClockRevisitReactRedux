a593b751c68d9d882fab006e20a998628002969e
htbkoo <h1017760@connect.hku.hk>
2018-01-27 08:31:10 +0800

    refactored Clock.js to eliminate duplicated code Pomodoro Clock (with React + Redux)

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -13,7 +13,9 @@ type Props = $ReadOnly<{
 // export const mapDispatchToProps = (state: State): DispatchProps => ({});
 export const ClockComponent = (props: Props): React$Element<any> => {
     let formGroups = [
-        {key: "name", labelSize: 1, controlSize: 10}
+        {key: "name", labelSize: 1, controlSize: 10},
+        {key: "duration", label: "Duration:", labelSize: 3, controlSize: 9},
+        {key: "colour", label: "Colour:", labelSize: 3, controlSize: 9}
     ].map(({key, label, labelSize, controlSize}: { key: string, label?: string, labelSize: number, controlSize: number }, index) => {
         let capitalizedKey = key.substring(0, 1).toUpperCase().concat(key.substring(1));
         return (
@@ -35,32 +37,6 @@ export const ClockComponent = (props: Props): React$Element<any> => {
         <div className="Clock">
             <Form horizontal>
                 {formGroups}
-                <FormGroup controlId="formHorizontalDuration" className="clocks-list-clock-duration">
-                    <Col componentClass={ControlLabel} sm={3}>
-                        Duration
-                    </Col>
-                    <Col sm={9}>
-                        <FormControl
-                            type="text"
-                            value={props.clock.duration}
-                            placeholder="Duration"
-                            onChange={NO_OP}
-                        />
-                    </Col>
-                </FormGroup>
-                <FormGroup controlId="formHorizontalColour" className="clocks-list-clock-colour">
-                    <Col componentClass={ControlLabel} sm={3}>
-                        Colour
-                    </Col>
-                    <Col sm={9}>
-                        <FormControl
-                            type="text"
-                            value={props.clock.colour}
-                            placeholder="Colour"
-                            onChange={NO_OP}
-                        />
-                    </Col>
-                </FormGroup>
             </Form>
         </div>
     )
