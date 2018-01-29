bcad3f427ca80362e0e7b7810b4e593477de89a5
Hey <htbkoo@users.noreply.github.com>
2018-01-29 20:02:43 +0800

    improved clocksHelper.getNextDuration to handle current clock is the last Pomodoro Clock (with React + Redux)

--- src/components/helpers/clocksHelper.js
+++ src/components/helpers/clocksHelper.js
@@ -6,7 +6,9 @@ export default {
     getNextDuration(clocks: Clocks, sessionClockId: ClockId): number {
         let allIds = clocks.allIds;
         let index = allIds.indexOf(sessionClockId);
-        let nextClockId = allIds[index + 1];
+        let isLastClock = (index === allIds.length - 1);
+        let nextClockId = isLastClock ? allIds[0] : allIds[index + 1];
+
         return clocks.byId[nextClockId].duration;
     }
 }
