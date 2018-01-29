cad966bbf96e6d6789a5d580cc1f18b6c0583eca
htbkoo <h1017760@connect.hku.hk>
2018-01-29 20:02:43 +0800

    improved clocksHelper.getNextDuration to handle current clock is the last Pomodoro Clock (with React + Redux)

--- src/components/helpers/clocksHelper.test.js
+++ src/components/helpers/clocksHelper.test.js
@@ -8,7 +8,25 @@ describe('clocksHelper', function () {
         it('should return the next clockId if the sessionClockId is not the last', function () {
             // given
             const sessionClockId = "1", expectedNextDuration = 200;
-            const clocks = new StateBuilder().addClock({id: sessionClockId, duration: 1}).addClock({id:"2", duration: expectedNextDuration}).build().clocks;
+            const clocks = new StateBuilder()
+                .addClock({id: sessionClockId, duration: 1})
+                .addClock({id: "2", duration: expectedNextDuration})
+                .build().clocks;
+
+            // when
+            let nextDuration = clocksHelper.getNextDuration(clocks, sessionClockId);
+
+            // then
+            expect(nextDuration).toEqual(expectedNextDuration);
+        });
+
+        it('should return the first clockId if the sessionClockId is the last', function () {
+            // given
+            const sessionClockId = "2", expectedNextDuration = 100;
+            const clocks = new StateBuilder()
+                .addClock({id: "1", duration: expectedNextDuration})
+                .addClock({id: sessionClockId, duration: 2})
+                .build().clocks;
 
             // when
             let nextDuration = clocksHelper.getNextDuration(clocks, sessionClockId);
