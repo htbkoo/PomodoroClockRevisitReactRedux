bc15ff29a181c355d85495f1a3240f2000eee08a
Hey <htbkoo@users.noreply.github.com>
2018-02-01 08:47:02 +0800

    improved StateBuilder; added StateBuilder.baseOn Pomodoro Clock (with React + Redux)

--- src/testUtils/StateBuilder.js
+++ src/testUtils/StateBuilder.js
@@ -1,70 +1,66 @@
-import type {ClockId, Clocks, Interval, State, WithClockId} from "../redux/state";
+import type {ClockId, Interval, State, WithClockId} from "../redux/state";
+
+import mergeOptions from "merge-options";
+
+const emptyState = {
+    interval: 0,
+    session: {
+        isCounting: false,
+        time: 0,
+        originalTime: 0,
+        clockId: "0"
+    },
+    clocks: {
+        byId: {},
+        allIds: []
+    }
+};
 
 export class StateBuilder {
     +withTime: (time: number) => StateBuilder;
-    +getTime: () => number;
     +withIsCounting: (isCounting: boolean) => StateBuilder;
-    +getIsCounting: () => boolean;
     +withInterval: (interval: Interval) => StateBuilder;
-    +getInterval: () => Interval;
     +withOriginalTime: (originalTime: number) => StateBuilder;
-    +getOriginalTime: () => number;
     +withSessionClockId: (clockId: ClockId) => StateBuilder;
-    +getSessionClockId: () => ClockId;
     +addClock: (clock: WithClockId) => StateBuilder;
-    +getClocks: () => Clocks;
 
-    constructor() {
-        let _time = 0, _isCounting = false, _interval: number = 0, _originalTime = 0, _sessionClockId = "0",
-            _clocks = {byId: {}, allIds: []};
-        this.withTime = time => {
-            _time = time;
-            return this;
-        };
-        this.getTime = () => _time;
-
-        this.withIsCounting = isCounting => {
-            _isCounting = isCounting;
-            return this;
-        };
-        this.getIsCounting = () => _isCounting;
-
-        this.withInterval = interval => {
-            _interval = interval;
-            return this;
-        };
-        this.getInterval = () => _interval;
-
-        this.withOriginalTime = originalTime => {
-            _originalTime = originalTime;
-            return this;
-        };
-        this.getOriginalTime = () => _originalTime;
-
-        this.withSessionClockId = sessionClockId => {
-            _sessionClockId = sessionClockId;
-            return this;
-        };
-        this.getSessionClockId = () => _sessionClockId;
-
-        this.addClock = clock => {
-            _clocks.byId[clock.id] = clock;
-            _clocks.allIds.push(clock.id);
-            return this;
-        };
-        this.getClocks = () => _clocks;
+    static baseOn(state: State): StateBuilder {
+        return new StateBuilder(state);
     }
 
-    build(): State {
-        return {
-            interval: this.getInterval(),
-            session: {
-                isCounting: this.getIsCounting(),
-                time: this.getTime(),
-                originalTime: this.getOriginalTime(),
-                clockId: this.getSessionClockId()
-            },
-            clocks: this.getClocks()
-        }
+    constructor(state: State = emptyState) {
+        let _state = mergeOptions(state);
+        this.withTime = time => {
+            _state.session.time = time;
+            return this;
+        };
+
+        this.withIsCounting = isCounting => {
+            _state.session.isCounting = isCounting;
+            return this;
+        };
+
+        this.withInterval = interval => {
+            _state.interval = interval;
+            return this;
+        };
+
+        this.withOriginalTime = originalTime => {
+            _state.session.originalTime = originalTime;
+            return this;
+        };
+
+        this.withSessionClockId = sessionClockId => {
+            _state.session.clockId = sessionClockId;
+            return this;
+        };
+
+        this.addClock = clock => {
+            _state.clocks.byId[clock.id] = clock;
+            _state.clocks.allIds.push(clock.id);
+            return this;
+        };
+
+        this.build = (): State => mergeOptions(_state);
     }
 }
