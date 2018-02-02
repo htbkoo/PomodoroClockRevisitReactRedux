60db8b59d6d787a4aaae6d86002c530f7b41d70b
Hey <htbkoo@users.noreply.github.com>
2018-02-02 08:37:27 +0800

    (WIP) passed props.originalTime to action.stopCounting in StopButton.js Pomodoro Clock (with React + Redux)

--- src/components/StopButton.js
+++ src/components/StopButton.js
@@ -3,7 +3,11 @@ import React from "react";
 import {stopCounting} from "../redux/actions";
 import SimpleButton from "./highOrderComponents/SimpleButton";
 
+type Props = {
+    +originalTime: number;
+}
+
 // Untested
-export default (): React$Element<any> => (
-    <SimpleButton action={stopCounting()} buttonId="btn_stop"/>
+export default (props: Props): React$Element<any> => (
+    <SimpleButton action={stopCounting(props.originalTime)} buttonId="btn_stop"/>
 );
