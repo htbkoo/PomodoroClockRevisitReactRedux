8634cd49705eadc15e732cad01df571206aad9fc
htbkoo <h1017760@connect.hku.hk>
2018-03-08 08:27:02 +0800

    spiked the usage of redux form Pomodoro Clock (with React + Redux)

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,10 +1,11 @@
 // @flow
 import React from "react";
-import {Col, ControlLabel, Form, FormControl, FormGroup} from "react-bootstrap";
+import {Col, ControlLabel, FormControl, FormGroup} from "react-bootstrap";
 
 import type {Clock} from "../redux/state";
 import {NO_OP} from "../utils/functionUtil";
 import type {onClockConfigUpdateType} from "./ClocksList";
+import {reduxForm, Field, FieldArray} from "redux-form";
 
 type Props = $ReadOnly<{
     +clock: Clock,
@@ -12,33 +13,109 @@ type Props = $ReadOnly<{
     +onClockConfigUpdate: onClockConfigUpdateType
 }>
 
-export const ClockComponent = (props: Props): React$Element<any> => {
-    let formGroups = [
-        {key: "name", labelSize: 1, controlSize: 10},
-        {key: "duration", label: "Duration:", labelSize: 3, controlSize: 9},
-        {key: "colour", label: "Colour:", labelSize: 3, controlSize: 9}
-    ].map((controlProps, index) => (
-            <ClockControlComponent controlProps={controlProps} clock={props.clock}
-                                   onClockConfigUpdate={props.onClockConfigUpdate}
-                                   key={index}/>
-        )
-    );
+const renderHobbies = ({fields, meta: {error}}) => (
+    <ul>
+        <li>
+            <button type="button" onClick={() => fields.push()}>
+                Add Hobby
+            </button>
+        </li>
+        {fields.map((hobby, index) => (
+            <li key={index}>
+                <button
+                    type="button"
+                    title="Remove Hobby"
+                    onClick={() => fields.remove(index)}
+                />
+                <Field
+                    name={hobby}
+                    type="text"
+                    component={renderField}
+                    label={`Hobby #${index + 1}`}
+                />
+            </li>
+        ))}
+        {error && <li className="error">{error}</li>}
+    </ul>
+)
 
+const renderMembers = ({fields, meta: {error, submitFailed}}) => (
+    <ul>
+        <li>
+            <button type="button" onClick={() => fields.push({})}>
+                Add Member
+            </button>
+            {submitFailed && error && <span>{error}</span>}
+        </li>
+        {fields.map((member, index) => (
+            <li key={index}>
+                <button
+                    type="button"
+                    title="Remove Member"
+                    onClick={() => fields.remove(index)}
+                />
+                <h4>Member #{index + 1}</h4>
+                <Field
+                    name={`${member}.firstName`}
+                    type="text"
+                    component={renderField}
+                    label="First Name"
+                />
+                <Field
+                    name={`${member}.lastName`}
+                    type="text"
+                    component={renderField}
+                    label="Last Name"
+                />
+                <FieldArray name={`${member}.hobbies`} component={renderHobbies}/>
+            </li>
+        ))}
+    </ul>
+)
+
+const FieldArraysForm = props => {
+    const {handleSubmit, pristine, reset, submitting} = props
     return (
-        <div className={getDivWrapperClasses().join(" ")}>
-            <Form horizontal>
-                {formGroups}
-            </Form>
+        <form onSubmit={handleSubmit}>
+            <Field
+                name="clubName"
+                type="text"
+                component={renderField}
+                label="Club Name"
+            />
+            <FieldArray name="members" component={renderMembers}/>
+            <div>
+                <button type="submit" disabled={submitting}>
+                    Submit
+                </button>
+                <button type="button" disabled={pristine || submitting} onClick={reset}>
+                    Clear Values
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export const ClockComponent = (props: Props): React$Element<any> => {
+    return (
+        <div>
+            {
+                [
+                    {key: "name", labelSize: 1, controlSize: 10},
+                    {key: "duration", label: "Duration:", labelSize: 3, controlSize: 9},
+                    {key: "colour", label: "Colour:", labelSize: 3, controlSize: 9}
+                ].map((controlProps, index) => (
+                        <Field clock={props.clock}
+                               controlProps={controlProps}
+                               onClockConfigUpdate={props.onClockConfigUpdate}
+                               key={index}
+                               name={`byId[${index}]`}
+                               component={ClockControlComponent}/>
+                    )
+                )
+            }
         </div>
     );
-
-    function getDivWrapperClasses() {
-        let array = ["Clock"];
-        if (props.isCurrent) {
-            array.push("Clock-current");
-        }
-        return array;
-    }
 };
 
 type ClockControlComponentProps = {
@@ -47,8 +124,50 @@ type ClockControlComponentProps = {
     +onClockConfigUpdate: onClockConfigUpdateType
 };
 
+const renderField = ({input, label, type, meta: {touched, error}}) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <input {...input} type={type} placeholder={label}/>
+            {touched && error && <span>{error}</span>}
+        </div>
+    </div>
+)
+
 export const ClockControlComponent = (props: ClockControlComponentProps): React$Element<any> => {
-    let {key, label, labelSize, controlSize} = props.controlProps;
+    let {input, clock, controlProps, meta: {touched, error}} = props;
+    let {key, label, labelSize, controlSize} = controlProps;
+
+    console.log(JSON.stringify(props));
+
+
+    let capitalizedKey = key.substring(0, 1).toUpperCase().concat(key.substring(1));
+    // return (
+    //     <div>
+    //         <label>{label}</label>
+    //         <div>
+    //             <input {...input} type="text" placeholder={label}/>
+    //             {touched && error && <span>{error}</span>}
+    //         </div>
+    //     </div>
+    // );
+    return (
+        <FormGroup controlId={`formHorizontal${capitalizedKey}`} className={`clocks-list-clock-${key}`}>
+            <Col componentClass={ControlLabel} sm={labelSize}>{label}</Col>
+            <Col sm={controlSize}>
+                <FormControl
+                    type="text"
+                    value={props.clock[key]}
+                    placeholder={capitalizedKey}
+                    onChange={NO_OP}
+                />
+            </Col>
+        </FormGroup>
+    );
+};
+export const BackupClockControlComponent = (props: ClockControlComponentProps): React$Element<any> => {
+    let {input, controlProps, meta: {touched, error}} = props;
+    let {key, label, labelSize, controlSize} = controlProps;
 
     let capitalizedKey = key.substring(0, 1).toUpperCase().concat(key.substring(1));
     return (
@@ -66,4 +185,25 @@ export const ClockControlComponent = (props: ClockControlComponentProps): React$
     );
 };
 
-export default ClockComponent;
+// export const ClockWrapperFormComponent = ({isCurrent, pristine, reset, submitting}: Props): React$Element<any> => {
+export const ClockWrapperFormComponent = (props: Props): React$Element<any> => {
+    return (
+        <div className={getDivWrapperClasses().join(" ")}>
+            <form>
+                <FieldArray name="clocks" component={ClockComponent} props={props}/>
+            </form>
+        </div>
+    );
+
+    function getDivWrapperClasses() {
+        let array = ["Clock"];
+        if (props.isCurrent) {
+            array.push("Clock-current");
+        }
+        return array;
+    }
+};
+
+export default reduxForm({
+    form: 'clocksForm', // a unique identifier for this form
+})(ClockWrapperFormComponent);
